c5fd6a9041f0a026ab7e4ab4cca79ef7a39b1b7e
Jai Gupta <jai.g@supporthive.com>
2026-04-07 13:56:30 +0530

    Refactor notification serializers and views for improved structure and functionality; update permissions for team membership checks; fixing count view in navigation tab

--- backend/apps/notifications/urls.py
+++ backend/apps/notifications/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import list_notifications, mark_all_notifications_read, mark_notification_read, mark_notification_unread
+from .views import (
+    MarkAllNotificationsReadView,
+    MarkNotificationReadView,
+    MarkNotificationUnreadView,
+    NotificationListView,
+)
 
 
 urlpatterns = [
-    path('list/', list_notifications, name='list-notifications'),
-    path('<int:notification_id>/read/', mark_notification_read, name='mark-notification-read'),
-    path('<int:notification_id>/unread/', mark_notification_unread, name='mark-notification-unread'),
-    path('read-all/', mark_all_notifications_read, name='mark-all-notifications-read'),
+    path('list/', NotificationListView.as_view(), name='list-notifications'),
+    path('<int:notification_id>/read/', MarkNotificationReadView.as_view(), name='mark-notification-read'),
+    path('<int:notification_id>/unread/', MarkNotificationUnreadView.as_view(), name='mark-notification-unread'),
+    path('read-all/', MarkAllNotificationsReadView.as_view(), name='mark-all-notifications-read'),
 ]
